607c0d30c21f06ccc7e737dd30304e72852f031c
Krzysztof Molenda <kmolenda@wsei.edu.pl>
2019-09-10 18:26:48 +0200

    Wersja 1

---
+++ SolutionEmerytura/ConsoleAppEmerytura/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEmerytura
+{
+    class Program
+    {
+        const int wiekEmerytalny = 65;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aplikacja EMERYTURA");
+
+            Console.Write("Podaj imię: ");
+            string imie = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie}");
+
+            Console.Write("Podaj wiek: ");
+            int wiek = int.Parse(Console.ReadLine());
+            if (wiek < wiekEmerytalny)
+            {
+                Console.WriteLine($"Zostało Ci {wiekEmerytalny - wiek} lat do emerytury");
+            }
+            else
+            {
+                Console.WriteLine("Jesteś emerytem.");
+            }
+
+            //Console.ReadKey(); // oczekuje na naciśnięcie dowolnego klawisza
+        }
+    }
+}
